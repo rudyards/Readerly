43621bf084a3c68c506ac9e41bb813b9c4243bd8
rudyards <rudy@mightycheese.com>
2019-05-24 14:43:56 -0700

    code still broken, close to working though
    current error is about passport

--- server.js
+++ server.js
@@ -4,42 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var User = require('../models/user');
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOne({ 'googleId': profile.id }, function(err, user) {
-          if (err) return cb(err);
-          if (user) {
-            return cb(null, user);
-          } else {
-            var newUser = new User({
-              username: profile.displayName,
-              googleId: profile.id
-            });
-            newUser.save(function(err) {
-              if (err) return cb(err);
-              return cb(null, newUser);
-            });
-          }
-        });  
-    }}));
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
+require('dotenv').config();
 require('./config/database');
 require('./config/passport');
 
